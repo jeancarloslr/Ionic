66b248220fa34d5102f45f13286e77fbb43dadb7
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-05-28 02:07:40 -0300

    try

--- ApiCepIo/src/app/home/home.page.ts
+++ ApiCepIo/src/app/home/home.page.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { Observable } from 'rxjs';   
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  endereco: Observable<any> | undefined;
+
+  constructor(private api: ApiService) {}
+
+  cep: number | undefined;
+
+  ngOnInit() {
+    this.endereco = this.api.getEndereco();
+  }
+
+    buscar(){
+
+      console.log(this.endereco);
+
+     }  
 
 
 }
